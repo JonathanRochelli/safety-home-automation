26d6216551113678544b306c534af845bc8dfe4f
Jonathan ROCHELLI <jonathan.rochelli@gmail.com>
2020-01-02 14:04:48 +0100

    Adding web page

---
+++ WWW/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<title>Nautilus</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="A-style.css" rel="stylesheet" media="all" type="text/css"> 
+</head>
+
+<body class="pi-dashboard">
+
+	<H1>Tableau de bord maison 192.168.1.104 (nautilus)</h1>
+	 
+	<section class="video-surveillance">
+
+		<img name="Galaxy-Tab" class="stream" src="http://192.168.1.104:8082/?action=stream" width="600" height="450" alt="Live Feed" title="Galaxy Tab GT-P6210" />
+
+		<img name="Foscam-FI8905W" class="stream" src= "http://192.168.1.104:8081/?action=stream" width="600" height="450" title="Foscam FI8905W"/>
+		
+	</section>
+	
+	<section class="capteurs">
+
+		<h3>Autres informations</h3>
+		<p>A venir...</p>
+		
+	</section>
+ 
+</body>
+</html>
